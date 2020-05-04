c9b47ea5fb79d63ab8f33bf2868597ad7b4d362c
Vadim Slynko <vadim@slynko.rocks>
2020-05-04 22:30:12 +0200

    Filter requests. 20-6

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,16 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+
     let contents = fs::read_to_string("hello.html").unwrap();
 
     let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+    } else {
+        // some other request
+    }
 }
