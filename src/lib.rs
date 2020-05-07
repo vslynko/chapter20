5c12826f032516559f22b5052f79a047f9713079
Vadim Slynko <vadim@slynko.rocks>
2020-05-07 21:29:45 +0200

    Add threadpool worker stub. 20-14

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
 use std::thread;
 
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        Worker {
+            id,
+            thread: thread::spawn(|| { }),
+        }
+    }
+}
+
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -21,10 +35,12 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {}
-        ThreadPool { threads }
+        for id in 0..size {
+            workers.push(Worker::new(id))
+        }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
