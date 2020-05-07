9c3384dcea63464b93c494bbb84408b9e0828662
Vadim Slynko <vadim@slynko.rocks>
2020-05-07 21:13:47 +0200

    Add threads storage to threadpool. 20-14

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,35 @@
 use std::thread;
 
-pub struct ThreadPool;
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool of given size (threads number)
-    /// 
+    ///
     /// # Panics
-    /// 
+    ///
     /// The new function will panic if the size is zero.
     /// ```should_panic
     /// chapter20::ThreadPool::new(0);
     /// ```
-    /// 
+    ///
     /// # Examples
     /// ```
     /// chapter20::ThreadPool::new(4);
     /// ```
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        ThreadPool
+
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {}
+        ThreadPool { threads }
     }
 
     pub fn execute<F>(&self, f: F)
     where
-        F: FnOnce() + Send + 'static
+        F: FnOnce() + Send + 'static,
     {
         thread::spawn(f);
     }
