2c64d875a4527de22ae1fde0cf1157628e59c080
Vadim Slynko <vadim@slynko.rocks>
2020-05-07 21:49:28 +0200

    Inject channel receiver to each worker thread. 20-15,-16,-17,-18

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 
 struct Worker {
@@ -6,16 +9,21 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         Worker {
             id,
-            thread: thread::spawn(|| { }),
+            thread: thread::spawn(|| {
+                receiver;
+            }),
         }
     }
 }
 
+struct Job;
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -34,13 +42,14 @@ impl ThreadPool {
     /// ```
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
